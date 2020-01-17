4816791d9ab4859485cd809cb415c8bf87ae2855
weijian <weijian.yang@thoughtworks.com>
2020-01-17 10:33:13 +0800

    move integration test to a class

--- src/test/java/MarsRover/RoverIntegrationTest.java
+++ src/test/java/MarsRover/RoverIntegrationTest.java
@@ -2,32 +2,13 @@ package MarsRover;
 
 import MarsRover.domain.Direction;
 import MarsRover.domain.Rover;
-import MarsRover.domain.command.MoveCommand;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class RoverTest {
-
-    private static Object[][] roverForDirectionOnlyMoveOnce() {
-        return new Object[][] {
-                {new Rover(0, 1, Direction.N), Direction.N},
-                {new Rover(0, -1, Direction.S), Direction.S},
-                {new Rover(1, 0, Direction.E), Direction.E},
-                {new Rover(-1, 0, Direction.W), Direction.W}
-        };
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "roverForDirectionOnlyMoveOnce")
-    void should_moved_rover(Rover expected, Direction direction) {
-        Rover rover = new Rover(0, 0, direction);
-        rover.move(new MoveCommand());
-
-        assertEquals(expected, rover);
-    }
+public class RoverIntegrationTest {
 
     private static Object[][] roverForDirectionTurnLeftAndOnlyMoveOnce() {
         return new Object[][] {
